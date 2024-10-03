0711003ea77a48ccd858705afa462e517aa38d2e
Hystrix <2669731677@qq.com>
2024-10-03 14:59:27 +0800

    second

--- node.js
+++ node.js
@@ -7,6 +7,13 @@ const fs = require('fs');
 app.use(cors());
 // 创建数据库连接
   app.use(express.static(path.join(__dirname, './frontend')));
+  
+// 处理根路径
+app.get('/', (req, res) => {
+ 
+    res.redirect('/index.html');
+  
+})
 // const options = {
 //   key: fs.readFileSync('./ssl/register.key'),
 //   cert: fs.readFileSync('./ssl/register_princesscantdefend_com.crt'),
@@ -14,9 +21,9 @@ app.use(cors());
 // };
 
 const options = {
-  key: fs.readFileSync('./ssl/kofshangrila.key'),
-  cert: fs.readFileSync('./ssl/kofshangrila_xyz.crt'),
-  ca: fs.readFileSync('./ssl/kofshangrila_xyz.ca-bundle')
+  key: fs.readFileSync('./ssl/kof.key'),
+  cert: fs.readFileSync('./ssl/kof.crt'),
+  ca: fs.readFileSync('./ssl/kof.ca-bundle')
 };
 
 https.createServer(options, app).listen(443,()=>{console.log('Server is running on https://localhost:443');})
